d29b87990181fe4c6dcd64ad3dfa22deacda7b0f
Maurice Woods <mowoodsiii@gmail.com>
2017-02-10 23:05:52 -0700

    Finished anwering questions throughout the lab

--- 2017spring/CommsLab/Lab03/top_block.py
+++ 2017spring/CommsLab/Lab03/top_block.py
@@ -4,7 +4,7 @@
 # GNU Radio Python Flow Graph
 # Title: Lab 03
 # Author: Maurice Woods
-# Generated: Fri Feb 10 21:12:24 2017
+# Generated: Fri Feb 10 23:00:32 2017
 ##################################################
 
 if __name__ == '__main__':
@@ -19,16 +19,15 @@ if __name__ == '__main__':
 
 from PyQt4 import Qt
 from PyQt4.QtCore import QObject, pyqtSlot
+from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import eng_notation
-from gnuradio import filter
 from gnuradio import gr
 from gnuradio import qtgui
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
+from gnuradio.qtgui import Range, RangeWidget
 from optparse import OptionParser
-import numpy as np
-import ptfun  # embedded python module
 import sip
 import sys
 from gnuradio import qtgui
@@ -63,37 +62,41 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.sps = sps = 5
-        self.samp_rate = samp_rate = 10000
-        self.ptype = ptype = 'tri'
-        self.k = k = 5
-        self.beta = beta = 2.5
-        self.PTYPE = PTYPE = 'sinc'
+        self.waveform = waveform = 102
+        self.samp_rate = samp_rate = 32000
+        self.offset = offset = 0
+        self.f0 = f0 = 1000
 
         ##################################################
         # Blocks
         ##################################################
-        self._PTYPE_options = ('rect', 'tri', 'sinc', )
-        self._PTYPE_labels = ('Rectangular', 'Triangular', 'Sinc', )
-        self._PTYPE_group_box = Qt.QGroupBox("PTYPE")
-        self._PTYPE_box = Qt.QVBoxLayout()
+        self._waveform_options = (102, 103, 104, )
+        self._waveform_labels = ('Cosine', 'Rectangular', 'Triangular', )
+        self._waveform_group_box = Qt.QGroupBox("waveform")
+        self._waveform_box = Qt.QHBoxLayout()
         class variable_chooser_button_group(Qt.QButtonGroup):
             def __init__(self, parent=None):
                 Qt.QButtonGroup.__init__(self, parent)
             @pyqtSlot(int)
             def updateButtonChecked(self, button_id):
                 self.button(button_id).setChecked(True)
-        self._PTYPE_button_group = variable_chooser_button_group()
-        self._PTYPE_group_box.setLayout(self._PTYPE_box)
-        for i, label in enumerate(self._PTYPE_labels):
+        self._waveform_button_group = variable_chooser_button_group()
+        self._waveform_group_box.setLayout(self._waveform_box)
+        for i, label in enumerate(self._waveform_labels):
         	radio_button = Qt.QRadioButton(label)
-        	self._PTYPE_box.addWidget(radio_button)
-        	self._PTYPE_button_group.addButton(radio_button, i)
-        self._PTYPE_callback = lambda i: Qt.QMetaObject.invokeMethod(self._PTYPE_button_group, "updateButtonChecked", Qt.Q_ARG("int", self._PTYPE_options.index(i)))
-        self._PTYPE_callback(self.PTYPE)
-        self._PTYPE_button_group.buttonClicked[int].connect(
-        	lambda i: self.set_PTYPE(self._PTYPE_options[i]))
-        self.top_grid_layout.addWidget(self._PTYPE_group_box, 1,1,2,1)
+        	self._waveform_box.addWidget(radio_button)
+        	self._waveform_button_group.addButton(radio_button, i)
+        self._waveform_callback = lambda i: Qt.QMetaObject.invokeMethod(self._waveform_button_group, "updateButtonChecked", Qt.Q_ARG("int", self._waveform_options.index(i)))
+        self._waveform_callback(self.waveform)
+        self._waveform_button_group.buttonClicked[int].connect(
+        	lambda i: self.set_waveform(self._waveform_options[i]))
+        self.top_grid_layout.addWidget(self._waveform_group_box, 0,0,2,1)
+        self._offset_range = Range(-1, 1, 0.01, 0, 200)
+        self._offset_win = RangeWidget(self._offset_range, self.set_offset, "offset", "counter_slider", float)
+        self.top_grid_layout.addWidget(self._offset_win, 0,1,1,1)
+        self._f0_range = Range(-5000, 5000, 1, 1000, 200)
+        self._f0_win = RangeWidget(self._f0_range, self.set_f0, "f0", "counter_slider", float)
+        self.top_grid_layout.addWidget(self._f0_win, 1,1,1,1)
         self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
         	1024, #size
         	samp_rate, #samp_rate
@@ -105,7 +108,7 @@ class top_block(gr.top_block, Qt.QWidget):
 
         self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
 
-        self.qtgui_time_sink_x_0.enable_tags(-1, False)
+        self.qtgui_time_sink_x_0.enable_tags(-1, True)
         self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
         self.qtgui_time_sink_x_0.enable_autoscale(False)
         self.qtgui_time_sink_x_0.enable_grid(True)
@@ -140,50 +143,72 @@ class top_block(gr.top_block, Qt.QWidget):
             self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win, 1,0,1,1)
-        self.qtgui_sink_x_0 = qtgui.sink_f(
-        	1024, #fftsize
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win, 2,0,1,1)
+        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_f(
+        	8192, #size
         	firdes.WIN_BLACKMAN_hARRIS, #wintype
         	0, #fc
         	samp_rate, #bw
         	"", #name
-        	True, #plotfreq
-        	True, #plotwaterfall
-        	True, #plottime
-        	True, #plotconst
+        	1 #number of inputs
         )
-        self.qtgui_sink_x_0.set_update_time(1.0/10)
-        self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_sink_x_0_win, 0,2,2,1)
+        self.qtgui_freq_sink_x_0.set_update_time(0.10)
+        self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
+        self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
+        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
+        self.qtgui_freq_sink_x_0.enable_autoscale(False)
+        self.qtgui_freq_sink_x_0.enable_grid(True)
+        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
+        self.qtgui_freq_sink_x_0.enable_axis_labels(True)
+        self.qtgui_freq_sink_x_0.enable_control_panel(False)
 
-        self.qtgui_sink_x_0.enable_rf_freq(False)
+        if not True:
+          self.qtgui_freq_sink_x_0.disable_legend()
 
+        if "float" == "float" or "float" == "msg_float":
+          self.qtgui_freq_sink_x_0.set_plot_pos_half(not True)
 
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "dark blue"]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
 
-        self.interp_fir_filter_xxx_0 = filter.interp_fir_filter_fff(sps, (ptfun.pampt(sps,PTYPE,[k,beta,samp_rate])))
-        self.interp_fir_filter_xxx_0.declare_sample_delay(0)
-        self.blocks_vector_source_x_0 = blocks.vector_source_f(np.hstack((np.zeros(k),1,np.zeros(k))), True, 1, [])
+        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_freq_sink_x_0_win, 2,1,1,1)
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, waveform, f0, 1, offset)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_throttle_0, 0), (self.interp_fir_filter_xxx_0, 0))
-        self.connect((self.blocks_vector_source_x_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.interp_fir_filter_xxx_0, 0), (self.qtgui_sink_x_0, 0))
-        self.connect((self.interp_fir_filter_xxx_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.qtgui_time_sink_x_0, 0))
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "top_block")
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
 
-    def get_sps(self):
-        return self.sps
+    def get_waveform(self):
+        return self.waveform
 
-    def set_sps(self, sps):
-        self.sps = sps
-        self.interp_fir_filter_xxx_0.set_taps((ptfun.pampt(self.sps,self.PTYPE,[self.k,self.beta,self.samp_rate])))
+    def set_waveform(self, waveform):
+        self.waveform = waveform
+        self._waveform_callback(self.waveform)
+        self.analog_sig_source_x_0.set_waveform(self.waveform)
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -191,38 +216,23 @@ class top_block(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
-        self.qtgui_sink_x_0.set_frequency_range(0, self.samp_rate)
-        self.interp_fir_filter_xxx_0.set_taps((ptfun.pampt(self.sps,self.PTYPE,[self.k,self.beta,self.samp_rate])))
+        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
 
-    def get_ptype(self):
-        return self.ptype
+    def get_offset(self):
+        return self.offset
 
-    def set_ptype(self, ptype):
-        self.ptype = ptype
+    def set_offset(self, offset):
+        self.offset = offset
+        self.analog_sig_source_x_0.set_offset(self.offset)
 
-    def get_k(self):
-        return self.k
+    def get_f0(self):
+        return self.f0
 
-    def set_k(self, k):
-        self.k = k
-        self.interp_fir_filter_xxx_0.set_taps((ptfun.pampt(self.sps,self.PTYPE,[self.k,self.beta,self.samp_rate])))
-        self.blocks_vector_source_x_0.set_data(np.hstack((np.zeros(self.k),1,np.zeros(self.k))), [])
-
-    def get_beta(self):
-        return self.beta
-
-    def set_beta(self, beta):
-        self.beta = beta
-        self.interp_fir_filter_xxx_0.set_taps((ptfun.pampt(self.sps,self.PTYPE,[self.k,self.beta,self.samp_rate])))
-
-    def get_PTYPE(self):
-        return self.PTYPE
-
-    def set_PTYPE(self, PTYPE):
-        self.PTYPE = PTYPE
-        self._PTYPE_callback(self.PTYPE)
-        self.interp_fir_filter_xxx_0.set_taps((ptfun.pampt(self.sps,self.PTYPE,[self.k,self.beta,self.samp_rate])))
+    def set_f0(self, f0):
+        self.f0 = f0
+        self.analog_sig_source_x_0.set_frequency(self.f0)
 
 
 def main(top_block_cls=top_block, options=None):
